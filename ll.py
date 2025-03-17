f3e3b0176bbec70715fb53ed1d42ff8eef49999e
Roshni Gupta <roshni.g@turing.com>
2025-03-17 20:23:35 +0530

    creating a sample linked list

--- ll.py
+++ ll.py
@@ -52,4 +52,14 @@ def print_list(head):
     print(result)
 
 
-    
+# Creating a sample linked list [1,2,3,4,5]
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5    
