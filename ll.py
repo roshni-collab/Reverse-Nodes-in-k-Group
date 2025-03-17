b31954a1735912ee296215914292169fc391f9bf
Roshni Gupta <roshni.g@turing.com>
2025-03-17 20:13:40 +0530

    counting number oof nodes

--- ll.py
+++ ll.py
@@ -2,3 +2,13 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+def reverseKGroup(head, k):
+    if not head or k == 1:
+        return head  # No need to reverse if k is 1 or list is empty
+
+    count = 0
+    current = head
+    while current:
+        count += 1
+        current = current.next
